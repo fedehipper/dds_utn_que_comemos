29920acd6364698661a8ce9fed999b5cba8056ea
fsena92 <fedes_92@hotmail.com>
2015-04-10 00:10:45 -0300

    Update Venta.java
    Errores para compilar modificados

--- MacoWins2/src/main/java/macoWins/Venta.java
+++ MacoWins2/src/main/java/macoWins/Venta.java
@@ -1,24 +1,50 @@
 package macoWins;
 
+import macoWins.MacoWins;
+import macoWins.Prenda;
+
+
 public class Venta {
 	
 	private Prenda prendaVendida;
 	private int cantidadVendida;
 	private String fechaVenta;
 	
-	public Venta(Prenda prendaVendida, int cantidadVendida, String fechaVenta) {
-		
+	public Venta(Prenda prenda, int cantidad, String fecha){
+		prendaVendida = prenda;
+		cantidadVendida = cantidad;
+		fechaVenta = fecha;
+	}
+	
+	public double precio(MacoWins maco) { 
+		return ((prendaVendida.precioFinal(maco)) * cantidadVendida);
+	}
+	
+	public Prenda getPrendaVendida() {
+		return prendaVendida;
+	}
+	
+	public void setPrendaVendida(Prenda prendaVendida) {
 		this.prendaVendida = prendaVendida;
-		this.cantidadVendida = cantidadVendida;
+	}
+	
+	public int getCantidad() {
+		return cantidadVendida;
+	}
+	
+	public void setCantidad(int cantidad) {
+		this.cantidadVendida = cantidad;
+	}
+	
+	public String getFechaVenta() {
+		return fechaVenta;
+	}
+	
+	public void setFechaVenta(String fechaVenta) {
 		this.fechaVenta = fechaVenta;
 	}
+	
+	
 
-	public double precio() { 
-		return ((prendaVendida.precioFinal()) * cantidadVendida);
-	}
-
-	public boolean coincideFechaCon(String unaFecha) {
-		return fechaVenta.equals(unaFecha);
-	}
 
 }
