c206bbb1cd5ded70c29f7ba4c7c7cdb3a053dade
fedehipper <federicohipper@gmail.com>
2015-04-21 20:42:39 -0300

    Agregue un test de masa corporal

--- queComemos-3053-04/src/test/java/grupo4/dds/TestUsuario.java
+++ queComemos-3053-04/src/test/java/grupo4/dds/TestUsuario.java
@@ -6,13 +6,18 @@ import org.junit.Test;
 
 public class TestUsuario {
 
-    private Usuario fecheSena = new Usuario(1.70,65);
-    
+    private Usuario fecheSena = new Usuario(1.70, 65);
+    private Usuario federicoHipper = new Usuario(1.91, 102);
     
 		
 	@Test
-	public void testConPeso65YAltura() {
+	public void testConPeso65YAltura170() {
 		assertEquals(fecheSena.indiceDeMasaCorporal(), 38.235 , 0.001);
+	}
+	
+	@Test
+	public void testConPeso102YAltura191() {
+		assertEquals(federicoHipper.indiceDeMasaCorporal(), 53.403, 0.001);
 	}
 
 }
