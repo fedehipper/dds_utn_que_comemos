0561158e13cf976fed49e772bfbdb917958fb2b5
fedehipper <federicohipper@gmail.com>
2015-05-19 18:23:39 -0300

    agregue la excepcion de NO poder sugerir una receta a un grupo

--- queComemos-3053-04/src/main/java/grupo4/dds/excepciones/EsInadecuadaDespuesDeModificar.java
+++ queComemos-3053-04/src/main/java/grupo4/dds/excepciones/EsInadecuadaDespuesDeModificar.java
@@ -2,9 +2,6 @@ package grupo4.dds.excepciones;
 
 public class EsInadecuadaDespuesDeModificar extends NoSePuedeModificarLaReceta {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 345907314922319320L;
 
 }
