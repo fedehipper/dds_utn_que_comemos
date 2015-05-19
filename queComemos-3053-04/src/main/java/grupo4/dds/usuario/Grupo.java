97268e802ec656b16a49ce99b903fec2cae239ce
fedehipper <federicohipper@gmail.com>
2015-05-19 18:25:18 -0300

    agregue el metodo sugerir receta

---
+++ queComemos-3053-04/src/main/java/grupo4/dds/usuario/Grupo.java
@@ -0,0 +1,26 @@
+package grupo4.dds.usuario;
+
+import grupo4.dds.excepciones.NoSePuedeSugerirRecetaAlGrupo;
+import grupo4.dds.receta.Receta;
+
+import java.util.List;
+
+public class Grupo {
+	
+	private List<Usuario> usuarios;
+	private String nombreDelClub;
+	private List<Ingrediente> palabrasClave;
+	
+	
+	// testear
+	public void sugerirReceta(Receta unaReceta) {
+		if(!((unaReceta.compartenPalabrasClave(palabrasClave) & (usuarios.stream().allMatch(u -> u.esAdecuada(unaReceta))))))
+				throw new NoSePuedeSugerirRecetaAlGrupo();
+	}
+
+	
+	
+	
+	
+	
+}
