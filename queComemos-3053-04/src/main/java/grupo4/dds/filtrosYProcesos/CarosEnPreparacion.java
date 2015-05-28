49137f79f0c93a0910ffc679183d79c6583b57e7
fedehipper <federicohipper@gmail.com>
2015-05-28 01:16:22 -0300

    agregadas todas las clases de strategies, faltan los procesos posteriores

---
+++ queComemos-3053-04/src/main/java/grupo4/dds/filtrosYProcesos/CarosEnPreparacion.java
@@ -0,0 +1,29 @@
+package grupo4.dds.filtrosYProcesos;
+
+import grupo4.dds.receta.Receta;
+import grupo4.dds.receta.RepositorioDeRecetas;
+import grupo4.dds.usuario.Ingrediente;
+import grupo4.dds.usuario.Usuario;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CarosEnPreparacion {
+
+	private List<Ingrediente> ingredientesCaros = new ArrayList<>();
+	
+	public void filtrar(Usuario usuario, RepositorioDeRecetas repoRecetas) {
+		
+		List<Receta> filtroDeReceta = new ArrayList<>();
+		
+		filtroDeReceta = repoRecetas.listarRecetasParaUnUsuario(usuario).stream().filter(r -> r.interseccion(r.getIngredientes(),(this.ingredientesCaros)).isEmpty()).collect(Collectors.toList());
+		 
+		repoRecetas.actualizarConsultaDeRecetas(filtroDeReceta);
+	}
+	
+	public void setIngredienteCaro(Ingrediente unIngrediente) {
+		this.ingredientesCaros.add(unIngrediente);
+	}
+
+}
