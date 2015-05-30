99778112176034db69da0c19d5c1155931ab7f7a
fsena92 <fedes_92@hotmail.com>
2015-05-30 13:01:16 -0300

    -Agregada Excepcion NoSePuedeAgregarFiltro en RepositorioDeReceta

---
+++ queComemos-3053-04/src/main/java/grupo4/dds/excepciones/NoSePuedeAgregarFiltro.java
@@ -0,0 +1,7 @@
+package grupo4.dds.excepciones;
+
+public class NoSePuedeAgregarFiltro extends RuntimeException {
+	
+	private static final long serialVersionUID = -1759454148318625555L;
+
+}
--- queComemos-3053-04/src/main/java/grupo4/dds/receta/RepositorioDeRecetas.java
+++ queComemos-3053-04/src/main/java/grupo4/dds/receta/RepositorioDeRecetas.java
@@ -1,5 +1,6 @@
 package grupo4.dds.receta;
 
+import grupo4.dds.excepciones.NoSePuedeAgregarFiltro;
 import grupo4.dds.usuario.Usuario;
 
 import java.util.ArrayList;
@@ -9,8 +10,14 @@ import java.util.stream.Collectors;
 public class RepositorioDeRecetas implements Repositorio {
 
 	private List<Receta> recetas = new ArrayList<>();
+	private boolean procesoFinalizado = false;
 	
 	/* Servicios */
+	public RepositorioDeRecetas(){
+		if (procesoFinalizado)
+			throw new NoSePuedeAgregarFiltro();
+			
+	}
 	
 	public List<Receta> listarRecetasParaUnUsuario(Usuario unUsuario) {
 		return recetas.stream().filter(r -> unUsuario.puedeVer(r))
