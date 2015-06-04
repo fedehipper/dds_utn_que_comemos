7113e5048dbd1553b345378852049ff4cd085b68
fedehipper <federicohipper@gmail.com>
2015-06-04 12:45:57 -0300

    agregada la clase repoUsuarios

---
+++ queComemos-3053-04/src/main/java/grupo4/dds/usuario/RepoUsuarios.java
@@ -0,0 +1,12 @@
+package grupo4.dds.usuario;
+
+public class RepoUsuarios {
+	
+	public void add(Usuario usuario) {}
+	public void remove(Usuario usuario) {}
+	public void update(Usuario usuario) {}
+	public void list(Usuario usuario) {}
+	public void get(Usuario usuario) {}
+	
+
+}
