3978f3d2442e49481e04f1855f5fc45a806623ab
fedehipper <federicohipper@gmail.com>
2015-06-07 15:12:14 -0300

    agregado test y correcciones

--- queComemos-3053-04/src/main/java/grupo4/dds/monitores/RecetaMasConsultada.java
+++ queComemos-3053-04/src/main/java/grupo4/dds/monitores/RecetaMasConsultada.java
@@ -7,22 +7,46 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.ListIterator;
 
-public class RecetasMasConsultadas implements Monitor {
+public class RecetaMasConsultada implements Monitor {
 
 	private HashMap<String, Integer> contadorDeRecetas = new HashMap<String, Integer>();
+	private String recetaMasConsultada;
+	private Integer cantidadDeConsultas = 0;
+	
 	
 	public void notificarConsulta(List<Receta> consulta, Usuario usuarioConsultor) {
 
-			ListIterator<Receta> r =  consulta.listIterator(); 
+		ListIterator<Receta> r =  consulta.listIterator(); 
 				
-			while(r.hasNext()) {
-				if (contadorDeRecetas.containsKey(r.next().getNombreDelPlato())) 
-					contadorDeRecetas.replace(r.next().getNombreDelPlato(), contadorDeRecetas.hashCode() + 1);
-				else
-					contadorDeRecetas.put(r.next().getNombreDelPlato(), 1);
-			}
-				
+		while(r.hasNext()) {
+			String nom = r.next().getNombreDelPlato();
+			
+			if (contadorDeRecetas.containsKey(nom)) 
+				contadorDeRecetas.put(nom, contadorDeRecetas.get(nom) + 1);
+			else
+				contadorDeRecetas.put(nom, 1);
+		}
+	}
+	
+	public HashMap<String, Integer> recetaMasConsultada() {
+		
+		contadorDeRecetas.forEach((r, v) -> guardarMaximo(v, r));
+		HashMap<String, Integer> receta = new HashMap<String, Integer>();
+		receta.put(recetaMasConsultada, cantidadDeConsultas);
+		return receta;
 	}
 
+	public void guardarMaximo(Integer unValor, String unaReceta) {
 		
+		if (unValor >= cantidadDeConsultas) {
+			cantidadDeConsultas = unValor;
+			recetaMasConsultada = unaReceta;
+		}
+	
+	}
+	
+	public String getRecetaMasConsultada() {
+		return recetaMasConsultada;
+	}
+	
 }
--- queComemos-3053-04/src/test/java/grupo4/dds/TestObserver.java
+++ queComemos-3053-04/src/test/java/grupo4/dds/TestObserver.java
@@ -3,12 +3,14 @@ package grupo4.dds;
 import static org.junit.Assert.*;
 
 import java.time.LocalTime;
+import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import grupo4.dds.monitores.CantidadDeHoras;
 import grupo4.dds.monitores.CantidadDeVeganos;
+import grupo4.dds.monitores.RecetaMasConsultada;
 import grupo4.dds.receta.EncabezadoDeReceta;
 import grupo4.dds.receta.Receta;
 import grupo4.dds.usuario.Usuario;
@@ -18,23 +20,28 @@ import org.junit.Test;
 
 
 public class TestObserver {
-	private EncabezadoDeReceta encabezadoDificil = new EncabezadoDeReceta(null, null, "D");
-	private EncabezadoDeReceta encabezadoFacil = new EncabezadoDeReceta(null, null, "F");
-	private Receta r1 = Receta.crearNueva(null, encabezadoDificil, null);
-	private Receta r2 = Receta.crearNueva(null, encabezadoDificil, null);
-	private Receta r3 = Receta.crearNueva(null, encabezadoDificil, null);
-	private Receta r4 = Receta.crearNueva(null, encabezadoFacil, null);
-	private Receta r5 = Receta.crearNueva(null, encabezadoFacil, null);
+	private static EncabezadoDeReceta encabezado1 = new EncabezadoDeReceta("fideo", null, "D");
+	private static EncabezadoDeReceta encabezado2 = new EncabezadoDeReceta("arroz", null, "D");
+	private static EncabezadoDeReceta encabezado3 = new EncabezadoDeReceta("lechon", null, "D");
+	private static EncabezadoDeReceta encabezado4 = new EncabezadoDeReceta("sopa", null, "F");
+	private static EncabezadoDeReceta encabezado5 = new EncabezadoDeReceta("milanga", null, "F");
 	
-	private List<Receta> l1 = Stream.of(r5, r4).collect(Collectors.toList());
-	private List<Receta> l2 = Stream.of(r1, r2, r3, r4).collect(Collectors.toList());
+	private static Receta r1 = Receta.crearNueva(null, encabezado1, null);
+	private static Receta r2 = Receta.crearNueva(null, encabezado2, null);
+	private static Receta r3 = Receta.crearNueva(null, encabezado3, null);
+	private static Receta r4 = Receta.crearNueva(null, encabezado4, null);
+	private static Receta r5 = Receta.crearNueva(null, encabezado5, null);
+	
+	private static List<Receta> l1 = Stream.of(r5, r4).collect(Collectors.toList());
+	private static List<Receta> l2 = Stream.of(r1, r2, r3, r4).collect(Collectors.toList());
 	private List<Receta> l3 = Stream.of(r3, r5).collect(Collectors.toList());
 	
-	private Usuario u = Usuario.crearPerfil();
+	private static Usuario u = Usuario.crearPerfil();
 	private CantidadDeHoras cantidadHoras = new CantidadDeHoras();
 	private CantidadDeVeganos cantidadVeganos = new CantidadDeVeganos();
 	private Vegano vegeno = new Vegano();
-	private Usuario Ariel = Usuario.crearPerfil();
+	private static Usuario Ariel = Usuario.crearPerfil();
+	private static RecetaMasConsultada recetaMasConsultada = new RecetaMasConsultada();
 	
 	
 	@Test
@@ -55,6 +62,36 @@ public class TestObserver {
 		
 		assertTrue(cantidadVeganos.getContadorDeVeganos() == 1);
 	}
+	
+	@Test
+	public void testCantidadConsultasDeUnaReceta() {
+		recetaMasConsultada.guardarMaximo(10, "fideos");
+		recetaMasConsultada.guardarMaximo(1, "pescado");
+		recetaMasConsultada.guardarMaximo(2, "fideos");
+		recetaMasConsultada.guardarMaximo(9, "arroz");
+		recetaMasConsultada.guardarMaximo(4, "fideos");
+		
+		assertEquals(recetaMasConsultada.getRecetaMasConsultada(), "fideos");
+	}
+	
+	@Test 
+	public void testDadoUnHashConRecetasYConsultasDevuelveLaMasConsultada() {
+		recetaMasConsultada.notificarConsulta(l2, Ariel);
+		recetaMasConsultada.notificarConsulta(l1, u);
+		recetaMasConsultada.notificarConsulta(l1, Ariel);
+		recetaMasConsultada.notificarConsulta(l2, u);
+		
+		HashMap<String, Integer> resultado = new HashMap<String, Integer>();
+		resultado.put("sopa", 4);
+		
+		assertEquals(recetaMasConsultada.recetaMasConsultada(), resultado);
+	}
+	
+	
+	
+	
+	
+	
 	
 	
 	
