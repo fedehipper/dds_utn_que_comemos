c1854414747b9f19af7dd3f81c7440412c7bc854
fedehipper <federicohipper@gmail.com>
2015-06-13 21:19:50 -0300

    refactor

--- queComemos-3053-04/src/main/java/grupo4/dds/monitores/RecetasMasConsultadas.java
+++ queComemos-3053-04/src/main/java/grupo4/dds/monitores/RecetasMasConsultadas.java
@@ -12,8 +12,7 @@ import java.util.stream.Collectors;
 
 public class RecetasMasConsultadas implements Monitor {
 	
-
-	private HashMap<Receta, Integer> contadorDeRecetas = new HashMap<Receta, Integer>();
+	private HashMap<Receta, Integer> contadorRecetas = new HashMap<Receta, Integer>();
 	
 
 	public void notificarConsulta(List<Receta> consulta, Usuario usuarioConsultor) {
@@ -22,27 +21,28 @@ public class RecetasMasConsultadas implements Monitor {
 				
 		while(receta.hasNext()) {
 			Receta aux = receta.next();
-			
-			if (contadorDeRecetas.containsKey(aux))
-				contadorDeRecetas.put(aux, contadorDeRecetas.get(aux) + 1);
+			if (contadorRecetas.containsKey(aux))
+				contadorRecetas.put(aux, contadorRecetas.get(aux) + 1);
 			else
-				contadorDeRecetas.put(aux, 1);
+				contadorRecetas.put(aux, 1);
 		}
 	}
 	
 	public HashMap<Receta, Integer> recetasMasConsultadas(int cantidad) {
 		
-		List<Receta> recetas = contadorDeRecetas.keySet().stream().collect(Collectors.toList());
-		List<Receta> vistaRecetasPorCantidad = ordenMasConsultadas(recetas).subList(0, cantidad);
-		
+		ListIterator<Receta> punteroReceta = recetasOrdenadas(cantidad).listIterator();
 		HashMap<Receta, Integer> recetasYCantidad = new HashMap<>();
-		ListIterator<Receta> punteroReceta = vistaRecetasPorCantidad.listIterator();
 		
 		while(punteroReceta.hasNext()) {
 			Receta unaReceta = punteroReceta.next();
-			recetasYCantidad.put(unaReceta, contadorDeRecetas.get(unaReceta));
+			recetasYCantidad.put(unaReceta, contadorRecetas.get(unaReceta));
 		}
 		return recetasYCantidad;
+	}
+	
+	public List<Receta> recetasOrdenadas(int cantidad) {
+		List<Receta> recetas = contadorRecetas.keySet().stream().collect(Collectors.toList());
+		return ordenMasConsultadas(recetas).subList(0, cantidad);
 	}
 	
 	public List<Receta> ordenMasConsultadas(List<Receta> recetas) {
@@ -51,7 +51,7 @@ public class RecetasMasConsultadas implements Monitor {
 	}
 	
 	public int getValor(Receta receta) {
-		return contadorDeRecetas.get(receta);
+		return contadorRecetas.get(receta);
 	}
 	
 }
