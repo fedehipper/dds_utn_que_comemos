c8a1485d33dc47fa93f67f8f59eea1cf52219965
Cristian <maldonadocristian2010@hotmail.com>
2015-06-24 21:31:25 -0300

    Agregada dependencia log4j a pom y loguearConsultas

---
+++ queComemos-3053-04/src/main/java/grupo4/dds/command/LoguearConsultas.java
@@ -0,0 +1,38 @@
+package grupo4.dds.command;
+
+import grupo4.dds.receta.RepositorioDeRecetas;
+import grupo4.dds.receta.busqueda.filtros.Filtro;
+import grupo4.dds.receta.busqueda.postProcesamiento.PostProcesamiento;
+import grupo4.dds.usuario.Usuario;
+
+import java.util.List;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+
+public class LoguearConsultas implements Command{
+
+	private Usuario usuario;
+	private int consultas = 0;
+	private Logger log = Logger.getLogger("ConsultasConMasDe100Resultados");
+	private RepositorioDeRecetas repositorio;
+	private PostProcesamiento postprocesamiento;
+	private List<Filtro> filtros;
+
+	
+	public void loguearConsultas(Usuario usuario){
+		this.usuario = usuario;
+	}
+
+	public void ejecutar() {
+		BasicConfigurator.configure();
+		consultas = usuario.recetasQuePuedeVer(repositorio,filtros,postprocesamiento).size();
+		if (consultas > 100){
+			if (log.isTraceEnabled()){
+				log.trace("MasDe100Resultados");				
+			}
+		}
+		
+	}
+
+}
