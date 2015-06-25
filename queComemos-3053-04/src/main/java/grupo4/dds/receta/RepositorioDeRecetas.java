1a15fa88cae01c95ea617ef2a7b85af75a985775
fedehipper <federicohipper@gmail.com>
2015-06-25 00:45:01 -0300

    comentario

--- queComemos-3053-04/src/main/java/grupo4/dds/receta/RepositorioDeRecetas.java
+++ queComemos-3053-04/src/main/java/grupo4/dds/receta/RepositorioDeRecetas.java
@@ -64,7 +64,8 @@ public class RepositorioDeRecetas {
 	
 		// aca cada command recibe los parametros que necesitan, no es que todos reciben los mismo y 
 		// usan los que requieren, lo que si es necesario es agregarle ese parametro en la firma de este metodo
-		// (ejecutarAcciones) por ahora solo reciben al usuario y la consulta
+		// (ejecutarAcciones) por ahora solo reciben al usuario y la consulta, y son nuevas instancias de cada uno
+		// porque son nuevos usuarios por consulta
 		
 		agregarAccion(new MarcarRecetasFavoritas(usuario, consulta));
 		agregarAccion(new LoguearConsultas(usuario));
