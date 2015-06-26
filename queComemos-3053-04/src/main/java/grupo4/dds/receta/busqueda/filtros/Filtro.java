64ede90fdaafb39c6047bbfa9fb88e03153d902e
fsena92 <fedes_92@hotmail.com>
2015-06-26 01:50:20 -0300

    -Merge

--- queComemos-3053-04/src/main/java/grupo4/dds/receta/busqueda/filtros/Filtro.java
+++ queComemos-3053-04/src/main/java/grupo4/dds/receta/busqueda/filtros/Filtro.java
@@ -6,10 +6,9 @@ import grupo4.dds.usuario.Usuario;
 public interface Filtro {
 	
 	public boolean test(Usuario u, Receta r);
-<<<<<<< HEAD
+
 	
 	public String getNombre();
-=======
->>>>>>> 4eb9aa0cf239cbc85e3b48e91a8c5540cffc0605
+
 	
 }
