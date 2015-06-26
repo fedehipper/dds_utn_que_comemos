dd185978432b74f0773321633332ac08e0b0795f
fedehipper <federicohipper@gmail.com>
2015-06-26 02:19:07 -0300

    correcciones

--- queComemos-3053-04/src/test/java/grupo4/dds/TestCommand.java
+++ queComemos-3053-04/src/test/java/grupo4/dds/TestCommand.java
@@ -7,15 +7,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import grupo4.dds.command.Mail;
 import grupo4.dds.receta.EncabezadoDeReceta;
 import grupo4.dds.receta.Receta;
 import grupo4.dds.receta.RecetaPublica;
 import grupo4.dds.receta.RepositorioDeRecetas;
-import grupo4.dds.receta.busqueda.filtros.Filtro;
-import grupo4.dds.receta.busqueda.filtros.FiltroExcesoCalorias;
-import grupo4.dds.receta.busqueda.filtros.FiltroNoEsAdecuada;
-import grupo4.dds.receta.busqueda.filtros.FiltroNoLeGusta;
+
 import grupo4.dds.usuario.Usuario;
 
 import org.junit.Before;
@@ -27,7 +23,6 @@ import queComemos.entrega3.dominio.Dificultad;
 public class TestCommand {
 		
 	private Usuario federicoHipper;
-	private Usuario fecheSena;
 	private RepositorioDeRecetas repositorio = RepositorioDeRecetas.get();
 	
 	private Receta receta2;
@@ -36,11 +31,6 @@ public class TestCommand {
 	private RecetaPublica receta7;
 	private RecetaPublica receta8;
 	
-	private FiltroExcesoCalorias filtroExceso;
-	private FiltroNoEsAdecuada filtroInadecuada;
-	private FiltroNoLeGusta filtroNoLeGusta;
-	
-	private List<Filtro> filtros = new ArrayList<Filtro>();
 	private List<Receta> consulta = new ArrayList<Receta>();
 	
 	
@@ -49,20 +39,13 @@ public class TestCommand {
 		repositorio.vaciar();
 		
 		federicoHipper = Usuario.crearPerfil("Federico Hipperdinger", null, null, 1.91f, 99.0f, null, true, null);
-		fecheSena = Usuario.crearPerfil("FecheSena", null, null, 1.74f, 80.0f, null, true, "fesena92@gmail.com");
-
 		receta2 = Receta.crearNueva(federicoHipper, new EncabezadoDeReceta("receta2", null, Dificultad.DIFICIL, 300), null);
 		receta3 = Receta.crearNueva(federicoHipper, new EncabezadoDeReceta("receta3", null, null, 600), null);
 		receta6 = RecetaPublica.crearNueva(new EncabezadoDeReceta("receta6", null, null, 200), null);
 		receta7 = RecetaPublica.crearNueva(new EncabezadoDeReceta("receta7", null, null, 300), null);
 		receta8 = RecetaPublica.crearNueva(new EncabezadoDeReceta("receta8", null, null, 100), null);
 		
-		filtroExceso = new FiltroExcesoCalorias();
-		filtroInadecuada = new FiltroNoEsAdecuada();
-		filtroNoLeGusta = new FiltroNoLeGusta();
-			
 		consulta = Arrays.asList(receta2, receta3, receta6, receta7, receta8);
-		filtros = Arrays.asList(filtroExceso, filtroInadecuada, filtroNoLeGusta);
 	}
 	
 
