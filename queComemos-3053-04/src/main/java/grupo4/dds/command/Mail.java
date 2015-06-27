87e2998e1428fa5b6bd4a5035972e9adfe0bb9c2
fsena92 <fedes_92@hotmail.com>
2015-06-26 23:51:11 -0300

    -Borrando lo viejo de mailSender

--- queComemos-3053-04/src/main/java/grupo4/dds/command/Mail.java
+++ queComemos-3053-04/src/main/java/grupo4/dds/command/Mail.java
@@ -22,64 +22,11 @@ import javax.mail.internet.MimeMessage;
 
 public class Mail {
 
-	private  String localMail = "queComemos4@gmail.com";
-	private  String pass = "ESCUADRONSUICIDA";
+
 
 	public void enviarMail(Usuario usuario, List<Receta> consulta,
 			List<Filtro> filtros) {
 
-		try {
-			// Propiedades de la conexión
-			Properties propiedades = configurarConexion(usuario);
-
-			// Inicializacion de la sesion
-			Session session = Session.getDefaultInstance(propiedades);
-
-			// Construcción del mensaje
-			MimeMessage mensajeCompleto = crearMensaje(session, usuario, consulta,filtros);
-
-			// Envío de mensaje.
-			Transport t = session.getTransport("smtp");
-			t.connect(localMail, pass);
-			t.sendMessage(mensajeCompleto, mensajeCompleto.getAllRecipients());
-
-			
-			
-			t.close();
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
 	}
-
-	public MimeMessage crearMensaje(Session session,Usuario usuario, List<Receta> consulta,
-			List<Filtro> filtros) throws MessagingException {
-		
-		MimeMessage mensaje = new MimeMessage(session);
-		mensaje.setFrom(new InternetAddress(localMail));
-		mensaje.addRecipient(Message.RecipientType.TO, new InternetAddress(usuario.getMail()));
-
-		mensaje.setSubject("QueComemos Consultas");
-
-		String consultaTexto = consulta.stream().map(c -> c.getNombreDelPlato()).collect(Collectors.toList()).toString();
-		String consultaFiltro = filtros.stream().map(f -> f.getNombre()).collect(Collectors.toList()).toString();
-		
-		mensaje.setText("Filtros Seleccionados: " + consultaFiltro + "\nConsultas: " + consultaTexto + "\n");
-		return mensaje;
-	}
-
-	public Properties configurarConexion(Usuario usuario) {
-		
-		Properties propiedades = new Properties();
-		propiedades.setProperty("mail.smtp.host", "smtp.gmail.com");
-		propiedades.setProperty("mail.smtp.starttls.enable", "true");
-		propiedades.setProperty("mail.smtp.port", "587");
-		propiedades.setProperty("mail.smtp.user", usuario.getMail());
-		propiedades.setProperty("mail.smtp.auth", "true");
-
-		return propiedades;
-	}
-	
 	
 }
