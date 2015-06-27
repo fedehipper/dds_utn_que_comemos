8d21742add3290c5625c54d0963e7fb2eb01e27c
fsena92 <fedes_92@hotmail.com>
2015-06-26 23:51:35 -0300

    -Agregado CommandMailSender

--- queComemos-3053-04/src/main/java/grupo4/dds/command/CommandMailSender.java
+++ queComemos-3053-04/src/main/java/grupo4/dds/command/CommandMailSender.java
@@ -7,24 +7,14 @@ import grupo4.dds.usuario.Usuario;
 import java.util.List;
 
 
-public class MailSender implements Command {
+public class CommandMailSender implements Command {
 
-	private Usuario usuario;
-	private List<Receta> consulta;
-	private List<Filtro> filtros;
 
-	public MailSender(Usuario usuario, List<Receta> consulta,
-			List<Filtro> filtros) {
 
-		this.usuario = usuario;
-		this.consulta = consulta;
-		this.filtros = filtros;
-
-	}
 
 	public void ejecutar()  {
 		Mail mail = new Mail();
-		mail.enviarMail(usuario, consulta, filtros);
+		//mail.enviarMail(usuario, consulta, filtros);
 	}
 
 }
