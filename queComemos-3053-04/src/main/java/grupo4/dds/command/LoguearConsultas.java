994b42fe04788689b8cf8b89f95732ed4eb65e75
Cristian <maldonadocristian2010@hotmail.com>
2015-06-27 22:15:29 -0300

    Corregido log4j.properties

--- queComemos-3053-04/src/main/java/grupo4/dds/command/LoguearConsultas.java
+++ queComemos-3053-04/src/main/java/grupo4/dds/command/LoguearConsultas.java
@@ -2,12 +2,9 @@ package grupo4.dds.command;
 
 import grupo4.dds.receta.Receta;
 import grupo4.dds.usuario.Usuario;
-
-import java.net.URL;
 import java.util.List;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
-import org.apache.log4j.helpers.Loader;
 
 public class LoguearConsultas implements Command{
 
@@ -19,8 +16,7 @@ public class LoguearConsultas implements Command{
 	}
 
 	public void ejecutar(Usuario usuario) {
-		URL url = Loader.getResource("log4j.properties");
-		PropertyConfigurator.configure(url);
+		PropertyConfigurator.configure("log4j.properties");
 		if (consultas.size() > 100){
 			if (log.isInfoEnabled())
 				log.info("Consultas Con Mas De 100 Resultados");	
