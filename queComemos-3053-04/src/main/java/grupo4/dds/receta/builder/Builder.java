6865a8ee5c629f101f8654ceb1aff6866586e422
unknown <ariel.folinovilardo@hotmail.com>
2015-06-30 23:29:50 -0300

    Borrado codigo comentado Agregada documentacion pto 2

--- queComemos-3053-04/src/main/java/grupo4/dds/receta/builder/Builder.java
+++ queComemos-3053-04/src/main/java/grupo4/dds/receta/builder/Builder.java
@@ -12,58 +12,58 @@ public abstract class Builder {
 	protected Receta receta;
 	protected BuilderEncabezado encabezado=new BuilderEncabezado();
 	
-	public Builder setCreador(Usuario creador/*,Receta receta*/) {
+	public Builder setCreador(Usuario creador) {
 		receta.setCreador(creador);
 		return this;
 	}
 
-	public Builder setTotalCalorias(int totalCalorias/*,BuilderEncabezado encabezado*/) {
+	public Builder setTotalCalorias(int totalCalorias) {
 		encabezado.setTotalCalorias(totalCalorias);
 		return this;
 	}
 
-	public Builder setNombreDelPlato(String nombreDelPlato/*,BuilderEncabezado encabezado*/) {
+	public Builder setNombreDelPlato(String nombreDelPlato) {
 		encabezado.setNombreDelPlato(nombreDelPlato);
 		return this;
 	}
 
-	public Builder setTemporada(Temporada temporada/*,BuilderEncabezado encabezado*/) {
+	public Builder setTemporada(Temporada temporada) {
 		encabezado.setTemporada(temporada);
 		return this;
 	}
 
-	public Builder setDificultad(Dificultad dificultad/*,BuilderEncabezado encabezado*/) {
+	public Builder setDificultad(Dificultad dificultad) {
 		encabezado.setDificultad(dificultad);
 		return this;
 	}
 
-	public Builder setIngrediente(Ingrediente unIngrediente/*,Receta receta*/) {
+	public Builder setIngrediente(Ingrediente unIngrediente) {
 		receta.agregarIngrediente(unIngrediente);
 		return this;
 	}
 
-	public Builder setCondimento(Ingrediente unCondimento/*,Receta receta*/) {
+	public Builder setCondimento(Ingrediente unCondimento) {
 		receta.agregarCondimento(unCondimento);
 		return this;
 	}
 
-	public Builder setSubreceta(Receta subreceta/*,Receta receta*/) {
+	public Builder setSubreceta(Receta subreceta) {
 		receta.agregarSubreceta(subreceta);
 		return this;
 	}
 
-	public Builder setPreparacion(String preparacion/*,Receta receta*/) {
+	public Builder setPreparacion(String preparacion) {
 		receta.setPreparacion(preparacion);
 		return this;
 	}
 
-	public Builder setEncabezado(EncabezadoDeReceta encabezado/*,Receta receta*/) {
+	public Builder setEncabezado(EncabezadoDeReceta encabezado) {
 		receta.setEncabezado(encabezado);
 		return this;
 	}
 
-	public Receta build(/*BuilderEncabezado encabezado,Receta receta*/) {	
-		setEncabezado(encabezado.build()/*,receta*/);
+	public Receta build() {	
+		setEncabezado(encabezado.build());
 	
 		if (!receta.esValida()){
 			throw new RuntimeException();
--- queComemos-3053-04/src/main/java/grupo4/dds/receta/builder/BuilderReceta.java
+++ queComemos-3053-04/src/main/java/grupo4/dds/receta/builder/BuilderReceta.java
@@ -2,14 +2,7 @@ package grupo4.dds.receta.builder;
 
 import grupo4.dds.receta.Receta;
 
-/*import queComemos.entrega3.dominio.Dificultad;
-import grupo4.dds.receta.EncabezadoDeReceta;
-import grupo4.dds.receta.Ingrediente;
 
-import grupo4.dds.receta.Temporada;
-import grupo4.dds.receta.builder.BuilderEncabezado;
-import grupo4.dds.usuario.Usuario;
-import grupo4.dds.receta.builder.*;*/
 
 
 public class BuilderReceta extends Builder {
@@ -17,56 +10,5 @@ public class BuilderReceta extends Builder {
 	public BuilderReceta(){
 		this.receta = new Receta();
 	}
-	/*
-	public BuilderReceta setCreador(Usuario creador) {
-		super.setCreador(creador, receta);
-		return this;
-	}
-
-	public BuilderReceta setTotalCalorias(int totalCalorias) {
-		super.setTotalCalorias(totalCalorias,encabezado);
-		return this;
-	}
-
-	public BuilderReceta setNombreDelPlato(String nombreDelPlato) {
-		super.setNombreDelPlato(nombreDelPlato,encabezado);
-		return this;
-	}
-
-	public BuilderReceta setTemporada(Temporada temporada) {
-		super.setTemporada(temporada,encabezado);
-		return this;
-	}
-
-	public BuilderReceta setDificultad(Dificultad dificultad) {
-		super.setDificultad(dificultad,encabezado);
-		return this;
-	}
-
-	public BuilderReceta setIngrediente(Ingrediente unIngrediente) {
-		super.setIngrediente(unIngrediente, receta);
-		return this;
-	}
-
-	public BuilderReceta setCondimento(Ingrediente unCondimento) {
-		super.setCondimento(unCondimento, receta);
-		return this;
-	}
-
-	public BuilderReceta setSubreceta(Receta subreceta) {
-		super.setSubreceta(subreceta,receta);
-		return this;
-	}
-
-	public BuilderReceta setPreparacion(String preparacion) {
-		super.setPreparacion(preparacion, receta);
-		return this;
-	}
-
-
-	public Receta build() {	
-		return super.build(encabezado, receta);
-
-	}
-*/
+	
 }
