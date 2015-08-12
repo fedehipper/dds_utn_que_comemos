f33fd7e04c67c7e8f237c3e9708cfcb5afb187f6
Matias Martino <matiasfmartino@gmail.com>
2015-08-12 15:32:01 -0300

    Corrigiendo pequeño feature envy

--- queComemos-3053-04/src/main/java/grupo4/dds/monitores/CantidadDeVeganos.java
+++ queComemos-3053-04/src/main/java/grupo4/dds/monitores/CantidadDeVeganos.java
@@ -5,14 +5,12 @@ import grupo4.dds.usuario.Usuario;
 
 import java.util.List;
 
-import queComemos.entrega3.dominio.Dificultad;
-
 public class CantidadDeVeganos implements Monitor {
 
 	private int contadorDeVeganos = 0;
 	
 	public void notificarConsulta(List<Receta> consulta, Usuario usuario) {
-		if(consulta.stream().anyMatch(r -> r.getDificultad() == Dificultad.DIFICIL) && usuario.esVegano()) 
+		if(consulta.stream().anyMatch(Receta::esDificil) && usuario.esVegano()) 
 			contadorDeVeganos++;
 	}
 	
