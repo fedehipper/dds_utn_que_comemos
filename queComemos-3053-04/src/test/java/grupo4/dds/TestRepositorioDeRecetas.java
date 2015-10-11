9ef96d38b0c78d303b75c8a9a686794017b15ce7
Matías Martino <matiasfmartino@gmail.com>
2015-10-11 00:25:06 -0300

    Refactor TestRepositorioDeRecetas

--- queComemos-3053-04/src/test/java/grupo4/dds/TestRepositorioDeRecetas.java
+++ queComemos-3053-04/src/test/java/grupo4/dds/TestRepositorioDeRecetas.java
@@ -1,12 +1,11 @@
 package grupo4.dds;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import grupo4.dds.monitores.CantidadDeHoras;
 import grupo4.dds.monitores.CantidadDeVeganos;
-import grupo4.dds.receta.EncabezadoDeReceta;
 import grupo4.dds.receta.Ingrediente;
 import grupo4.dds.receta.Receta;
-import grupo4.dds.receta.RecetaPublica;
 import grupo4.dds.receta.busqueda.filtros.Filtro;
 import grupo4.dds.receta.busqueda.filtros.FiltroExcesoCalorias;
 import grupo4.dds.receta.busqueda.filtros.FiltroNoEsAdecuada;
@@ -14,8 +13,6 @@ import grupo4.dds.receta.busqueda.filtros.FiltroNoLeGusta;
 import grupo4.dds.receta.busqueda.filtros.FiltroRecetasCaras;
 import grupo4.dds.receta.busqueda.postProcesamiento.Ordenar;
 import grupo4.dds.repositorios.RepositorioDeRecetas;
-import grupo4.dds.usuario.GrupoUsuarios;
-import grupo4.dds.usuario.Usuario;
 import grupo4.dds.usuario.condicion.Vegano;
 
 import java.time.LocalTime;
@@ -26,47 +23,24 @@ import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 
-import queComemos.entrega3.dominio.Dificultad;
-
 public class TestRepositorioDeRecetas extends BaseTest {
+
+	private RepositorioDeRecetas repositorio = RepositorioDeRecetas.get();
 	
-	private Usuario fecheSena;
-	private Usuario raul;
-	private Usuario arielFolino;
-	private Usuario matiasMartino;
-	private Usuario federicoHipper;
-	
-	private CantidadDeHoras cantidadHoras = new CantidadDeHoras();
 	private CantidadDeVeganos cantidadVeganos = new CantidadDeVeganos();
+	private CantidadDeHoras cantidadHoras = new CantidadDeHoras();
+	
 	private List<Receta> expected;
 	private List<Receta> consulta;
 	private List<Filtro> filtros;
-	private RepositorioDeRecetas repositorio = RepositorioDeRecetas.get();
-	
-	private Receta receta1;
-	private Receta receta2;
-	private Receta receta3;
-	private Receta receta4;
-	private Receta receta5;
-	private RecetaPublica receta6;
-	private RecetaPublica receta7;
-	private RecetaPublica receta8;
 	
 	@Before
 	public void setUp() {
+		
 		expected = null;
 		filtros = new ArrayList<>();
 		consulta = new ArrayList<>();
 		
-		raul = Usuario.crearPerfil("Raul", null, null, 1.70f, 65.0f, null, true, null);
-		fecheSena = Usuario.crearPerfil("Feche Sena", null, null, 1.70f, 65.0f, null, false, null);
-		arielFolino = Usuario.crearPerfil("Ariel Folino", null, null, 1.69f, 96.0f, null, true, null);
-		matiasMartino = Usuario.crearPerfil("Matías Martino", null, null, 1.74f, 79.0f, null, false, null);
-		federicoHipper = Usuario.crearPerfil("Federico Hipperdinger", null, null, 1.91f, 99.0f, null, false, null);
-		
-		GrupoUsuarios grupo1 = GrupoUsuarios.crearGrupo("grupo1");
-		GrupoUsuarios grupo2 = GrupoUsuarios.crearGrupo("grupo2");
-				
 		fecheSena.agregarGrupo(grupo1);
 		federicoHipper.agregarGrupo(grupo2);
 		arielFolino.agregarGrupo(grupo1);
@@ -75,23 +49,9 @@ public class TestRepositorioDeRecetas extends BaseTest {
 		fecheSena.agregarCondicion(new Vegano());
 		fecheSena.agregarComidaQueLeDisgusta(Ingrediente.nuevaComida("coliflor"));
 		
-		receta1 = Receta.crearNueva(fecheSena, new EncabezadoDeReceta("receta1", null, Dificultad.DIFICIL, 999), null);
-		receta2 = Receta.crearNueva(federicoHipper, new EncabezadoDeReceta("receta2", null, Dificultad.DIFICIL, 300), null);
-		receta3 = Receta.crearNueva(federicoHipper, new EncabezadoDeReceta("receta3", null, null, 600), null);
-		receta4 = Receta.crearNueva(arielFolino, new EncabezadoDeReceta("receta4", null, null, 100), null);
-		receta5 = Receta.crearNueva(matiasMartino, new EncabezadoDeReceta("receta5", null, null, 499), null);
-		receta6 = RecetaPublica.crearNueva(new EncabezadoDeReceta("receta6", null, null, 200), null);
-		receta7 = RecetaPublica.crearNueva(new EncabezadoDeReceta("receta7", null, null, 300), null);
-		receta8 = RecetaPublica.crearNueva(new EncabezadoDeReceta("receta8", null, null, 100), null);
-		
-		receta1.agregarIngrediente(Ingrediente.nuevaComida(""));
-		receta2.agregarIngrediente(Ingrediente.nuevaComida(""));
-		receta3.agregarIngrediente(Ingrediente.nuevaComida(""));
-		receta4.agregarIngrediente(Ingrediente.nuevaComida(""));
-		receta5.agregarIngrediente(Ingrediente.nuevaComida(""));
-		receta6.agregarIngrediente(Ingrediente.nuevaComida("carne"));
-		receta7.agregarIngrediente(Ingrediente.nuevaComida("lomo"));
-		receta8.agregarIngrediente(Ingrediente.nuevaComida("coliflor"));
+		salmon.agregarIngrediente(Ingrediente.nuevaComida("carne"));
+		lomito.agregarIngrediente(Ingrediente.nuevaComida("lomo"));
+		coliflor.agregarIngrediente(Ingrediente.nuevaComida("coliflor"));
 	}
 	
 	@Test
@@ -100,14 +60,14 @@ public class TestRepositorioDeRecetas extends BaseTest {
 		
 		List<Receta> recetasQuePuedeVer = repositorio.listarRecetasPara(arielFolino, null, null);
 				
-		assertFalse(recetasQuePuedeVer.contains(receta2));
-		assertFalse(recetasQuePuedeVer.contains(receta3));
-		assertFalse(recetasQuePuedeVer.contains(receta5));
+		assertFalse(recetasQuePuedeVer.contains(pollo));
+		assertFalse(recetasQuePuedeVer.contains(pure));
+		assertFalse(recetasQuePuedeVer.contains(bife));
 	}
 	
 	@Test 
 	public void testLasRecetasQuePuedeVerUnUsuarioSonPublicasOCompartidasEnALgunoDeSusGrupos() {
-		expected = Arrays.asList(receta1, receta4, receta6, receta7, receta8);
+		expected = Arrays.asList(milanesa, sopa, salmon, lomito, coliflor);
 		List<Receta> recetas = repositorio.listarRecetasPara(arielFolino, null, null);
 		
 		assertTrue(expected.containsAll(recetas) && recetas.containsAll(expected) && recetas.size() == expected.size());
@@ -115,7 +75,7 @@ public class TestRepositorioDeRecetas extends BaseTest {
 	
 	@Test
 	public void testSiNoAplicoFiltrosNiPostProcesamientoObtengoTodasLasRecetasQuePuedeVerElUsuario() {
-		expected = Arrays.asList(receta1, receta4, receta6, receta7, receta8);
+		expected = Arrays.asList(milanesa, sopa, salmon, lomito, coliflor);
 		List<Receta> recetas = repositorio.listarRecetasPara(arielFolino, null, null);
 		
 		assertTrue(expected.containsAll(recetas) && recetas.containsAll(expected) && recetas.size() == expected.size());
@@ -132,7 +92,7 @@ public class TestRepositorioDeRecetas extends BaseTest {
 		filtros.add(new FiltroNoLeGusta());	
 		filtros.add(filtroRecetasCaras);	
 		
-		expected = Arrays.asList(receta4);
+		expected = Arrays.asList(sopa);
 		List<Receta> recetas = repositorio.listarRecetasPara(fecheSena, filtros, null);
 		assertTrue(expected.containsAll(recetas) && recetas.containsAll(expected) && recetas.size() == expected.size());
 	}
@@ -145,7 +105,8 @@ public class TestRepositorioDeRecetas extends BaseTest {
 		
 		Ordenar procesamiento = new Ordenar((a,b) -> a.getTotalCalorias() - b.getTotalCalorias());
 		
-		expected = Arrays.asList(receta4, receta7, receta1);
+		expected = Arrays.asList(sopa, lomito, milanesa);
+		System.out.println(repositorio.listarRecetasPara(fecheSena, filtros, procesamiento));
 		assertTrue(expected.containsAll(repositorio.listarRecetasPara(fecheSena, filtros, procesamiento)));
 	}
 	
@@ -168,10 +129,14 @@ public class TestRepositorioDeRecetas extends BaseTest {
 	
 	@Test 
 	public void testMarcarFavoritasLasRecetasConsultadasParaUnUsuario() {
-		consulta = Arrays.asList(receta6, receta7, receta8);
-		RepositorioDeRecetas.get().listarRecetasPara(raul, null, null);
-		raul.ejecutarMarcadoPendiente();
-		assertTrue(raul.getHistorial().containsAll(consulta));
+		
+		consulta = Arrays.asList(salmon, lomito, coliflor);
+		
+		maria.setMarcaFavorita(true);
+		RepositorioDeRecetas.get().listarRecetasPara(maria, null, null);
+		maria.ejecutarMarcadoPendiente();
+		
+		assertTrue(maria.getHistorial().containsAll(consulta));
 	}
 	
 }
